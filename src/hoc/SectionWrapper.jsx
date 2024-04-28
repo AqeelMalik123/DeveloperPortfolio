596ea4b0e00cd2e4214349d95a9c068234116098
unknown <maqeel191@gmail.com>
2024-04-28 04:20:14 -0700

    changeWitdth

--- src/hoc/SectionWrapper.jsx
+++ src/hoc/SectionWrapper.jsx
@@ -2,7 +2,7 @@ import { motion } from "framer-motion";
 
 import { styles } from "../styles";
 import { staggerContainer } from "../utils/motion";
-
+//Ider sy meny width change ki real witdth ya thi max-w-7xl  after that me ya kardi thi meny w-full
 const StarWrapper = (Component, idName) =>
   function HOC() {
     return (
@@ -11,7 +11,7 @@ const StarWrapper = (Component, idName) =>
         initial='hidden'
         whileInView='show'
         viewport={{ once: true, amount: 0.25 }}
-        className={`${styles.padding} max-w-7xl mx-auto relative z-0`}
+        className={`${styles.padding} w-full mx-auto relative z-0`}
       >
         <span className='hash-span' id={idName}>
           &nbsp;
