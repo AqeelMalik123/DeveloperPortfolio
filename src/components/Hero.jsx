5790547f671d089688ca7248440ab46dd57a160d
unknown <maqeel191@gmail.com>
2024-08-19 12:06:18 -0700

    merge

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -19,7 +19,7 @@ const Hero = () => {
             Hi, I'm <span className='text-[#915EFF]'>Aqeel Malik</span>
           </h1>
           <p className={`${styles.heroSubText} mt-2 text-white-100`}>
-            I develop 3D visuals, user <br className='sm:block hidden' />
+            I develop React Js ande Next js , user <br className='sm:block hidden' />
             interfaces and web applications
           </p>
         </div>
