822ba3ad9a0236d814f2ad4dddb260400e31eb8e
unknown <maqeel191@gmail.com>
2024-12-13 12:14:16 -0800

    issue

--- src/App.jsx
+++ src/App.jsx
@@ -5,8 +5,8 @@ import { About, Contact, Experience, Feedbacks, Hero, Navbar, Tech, Works, Stars
 const App = () => {
   return (
     <BrowserRouter>
-      <div style={{width:"100%",border:"3px solid white"}} className='relative z-0 bg-primary w-full !important'>
-        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center'>
+      <div style={{width:"",backgroundColor:"white",padding:"",display:"",alignItems:"",justifyItems:""}} className='relative z-0 bg-primary w-full !important'>
+        <div className='bg-hero-pattern bg-cover bg-no-repeat bg-center' style={{display:"flex",width:"100%"}}>
           <Navbar />
           <Hero />
         </div>
